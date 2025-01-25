8b69db4ba908be9f06820b75d7541aaa5019be87
codeseight <159060026+codeseight@users.noreply.github.com>
2025-01-26 00:06:53 +0530

    ss

--- config/db.js
+++ config/db.js
@@ -4,14 +4,25 @@ const mongoUrl = 'mongodb+srv://tinkusaini1252:Tinku%4099@cluster0.dbsdk.mongodb
 
 const mongoDb = async () => {
     try {
-        await mongoose.connect(mongoUrl, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error.message);
+        await mongoose.connect(`mongodb+srv://tinkusaini1252:Tinku%4099@cluster0.dbsdk.mongodb.net/testing`);
+        // await  mongoose.connect("mongodb+srv://admin:admin@cluster0.xrh1grn.mongodb.net/Avatarlocals?retryWrites=true&w=majority&appName=Cluster0");
+        console.log("successfully connected");
+
     }
+    catch (error) {
+        console.log(error);
+
+
+    }
+    // try {
+    //     await mongoose.connect(mongoUrl, {
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true
+    //     });
+    //     console.log('Connected to MongoDB');
+    // } catch (error) {
+    //     console.error('Error connecting to MongoDB:', error.message);
+    // }
 };
 
 module.exports = mongoDb;
