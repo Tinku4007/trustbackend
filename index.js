37f0156500ded530abdc42f123b19d607d9f9022
codeseight <159060026+codeseight@users.noreply.github.com>
2025-01-26 01:03:09 +0530

    ss

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ app.use(express.json());
 // CORS configuration to allow all origins
 app.use(cors({
     origin: "*", // Allow all origins (make sure this is what you want)
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow specific HTTP methods including OPTIONS
-    allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods including OPTIONS
     credentials: true, // Allow credentials like cookies or authorization headers (if needed)
 }));
 
@@ -22,9 +21,8 @@ app.get("/", (req, res) => {
 
 app.use('/api', router);
 
-app.options('*', cors());
 
-const PORT = process.env.BACKEND_URL || 8000;
+const PORT = 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
