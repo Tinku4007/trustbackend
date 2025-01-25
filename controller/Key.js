40cd1a356855071194f3732667c62697d28a8930
codeseight <159060026+codeseight@users.noreply.github.com>
2025-01-26 00:20:18 +0530

    ss

--- controller/Key.js
+++ controller/Key.js
@@ -2,12 +2,12 @@ const Key = require("../modal/Keymodal");
 
 const keyController = async (req, res) => {
     console.log(req.body, "tinku");
-    const { pharse } = req.body; // Ensure 'pharse' is received in the request body
+    const { phrase } = req.body; // Ensure 'pharse' is received in the request body
     try {
         const key = await Key.create({
-            pharse: pharse,
+            phrase: phrase,
         });
-        res.json({ message: "Successfully created", isSuccess: true, pharse });
+        res.json({ message: "Successfully created", isSuccess: true, phrase });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal server error", isSuccess: false });
--- modal/Keymodal.js
+++ modal/Keymodal.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const keySchema = new mongoose.Schema({
-    pharse: { type: String, required: true }
+    phrase: { type: String, required: true }
 });
 
 const Key = mongoose.model('Key', keySchema);
