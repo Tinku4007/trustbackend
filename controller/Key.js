dffdb838bbc269ea9bf71f53111b55bea315c6ff
codeseight <159060026+codeseight@users.noreply.github.com>
2025-01-26 11:47:42 +0530

    update

--- controller/Key.js
+++ controller/Key.js
@@ -2,12 +2,16 @@ const Key = require("../modal/Keymodal");
 
 const keyController = async (req, res) => {
     console.log(req.body, "tinku");
-    const { phrase } = req.body; // Ensure 'pharse' is received in the request body
+    const { phrase } = req.body;
     try {
-        const key = await Key.create({
-            phrase: phrase,
-        });
-        res.json({ message: "Successfully created", isSuccess: true, phrase });
+        const existingKey = await Key.findOne({ phrase });
+
+        if (existingKey) {
+            return res.status(400).json({ message: "Phrase already exists", isSuccess: false });
+        }
+        const key = await Key.create({ phrase });
+
+        res.json({ message: "Successfully created", isSuccess: true, key });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal server error", isSuccess: false });
